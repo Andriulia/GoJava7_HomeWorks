c0999e5bd3be2f6fd52f19df866de692274280d9
Andriulia <andriuliaandriulia@gmail.com>
2017-05-14 00:32:55 +0300

    Add files via upload
    Correct

--- FiveDigitsSort1_1_2.java
+++ FiveDigitsSort1_1_2.java
@@ -1,41 +1,76 @@
-package com.Antrikos.HomeWork1_1_2;
+package com.Draft;
 
-import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Created by Avtpikoc on 04.05.2017.
+ * Created by Avtpikoc on 12.05.2017.
  */
-
 public class FiveDigitsSort1_1_2 {
     public static void main(String[] args) {
 
-        Object a5[] = new Object[5];
-        //
-
         System.out.println("Please enter 5 numbers with a space...");
-        Scanner scanner = new Scanner(System.in);
 
-        for(int i=0;i < a5.length; i++){
-            a5[i] = scanner.nextInt();
-        }
-
-
-        System.out.println("Initial array");
-        for (Object number : a5) {
-            System.out.print(number + ", ");
-        }
-
-        Arrays.sort(a5);
-
-        System.out.println();
-        System.out.println();
-        System.out.println("Sorted array");
-        for (Object number : a5) {
-            System.out.print(number + ", ");
+        for (;;) {
+            Exc();
+            continue;
         }
 
     }
+
+    public static void Exc() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            int a5[] = new int[5];
+
+            for(int i=0;i < a5.length; i++) {   // 1
+                a5[i] = scanner.nextInt();      // 1
+            }                                   // 1
+
+            System.out.println("Initial array:");
+            for (Object number : a5) {
+                System.out.print(number + ", ");
+                break;
+            }
+
+            System.out.println("Initial array:");
+            for (Object number : a5) {
+                System.out.print(number + ", ");
+            }
+
+            for (int i = 0; i < a5.length; i++) {
+                int min = a5[i];
+                int imin = i;
+                for (int j = i + 1; j < a5.length; j++) {
+                    if (a5[j] < min) {
+                        min = a5[j];
+                        imin = j;
+                    }
+                }
+                if (i != imin) {
+                    int temp = a5[i];
+                    a5[i] = a5[imin];
+                    a5[imin] = temp;
+                }
+            }
+
+
+            System.out.println();
+            System.out.println();
+            System.out.println("Sorted array");
+            for (Object number : a5) {
+                System.out.print(number + ", ");
+            }
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("Another input...");
+
+        }
+
+    }
+
 }
-
-
